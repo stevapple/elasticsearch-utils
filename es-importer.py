63fd0474e9fafcfe6479770cfb1326ee374e4ba9
YR Chen <stevapple@icloud.com>
2023-07-14 17:25:16 +0800

    Enhance readability and robustness

--- es-importer.py
+++ es-importer.py
@@ -141,14 +141,14 @@ if __name__ == '__main__':
     parser.add_argument('--port', default=9200, type=int, help='Elasticsearch port (default: 9200)')
     parser.add_argument('-u', '--username', default=None, help='Username for authentication')
     parser.add_argument('-p', '--password', default=None, help='Password for authentication')
-    parser.add_argument('--insecure', action='store_false', help='Use plain HTTP instead of HTTPS')
+    parser.add_argument('--insecure', action='store_true', help='Use plain HTTP instead of HTTPS')
     parser.add_argument('--ca-cert', default=None, help='Path to the CA certificate file')
     parser.add_argument('--pipeline', default=None, help='Name of the Elasticsearch pipeline')
-    parser.add_argument('--no-generate-action', action='store_false', help='Whether to generate action lines')
+    parser.add_argument('--no-generate-action', action='store_true', help='Whether to generate action lines')
     parser.add_argument('--id-field', default=None, type=str, help='Name of document ID field')
     parser.add_argument('-c', '--chunk_size', type=int, default=1000, help='Number of lines to process at once (default: 1000)')
     parser.add_argument('--dry-run', action='store_true', help='Print to stdout instead of sending to Elasticsearch')
     args = parser.parse_args()
 
-    asyncio.run(main(args.file_path, args.file_encoding, args.index, args.host, args.port, args.username, args.password, args.insecure, args.ca_cert,
-                     args.no_generate_action, args.id_field, args.pipeline, args.chunk_size, args.dry_run))
+    asyncio.run(main(args.file_path, args.file_encoding, args.index, args.host, args.port, args.username, args.password, not args.insecure, args.ca_cert,
+                     not args.no_generate_action, args.id_field, args.pipeline, args.chunk_size, args.dry_run))
